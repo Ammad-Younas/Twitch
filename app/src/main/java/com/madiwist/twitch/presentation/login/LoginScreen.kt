2f52db99a85d08f6bedf66a7c97c0d421d8d4c49
Muhammad Ammad Younas <ammadyounas837@gmail.com>
2026-06-21 23:03:13 +0500

    Login Screen Keyboard Covering Input Filed Issue Fixed

--- app/src/main/java/com/madiwist/twitch/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/madiwist/twitch/presentation/login/LoginScreen.kt
@@ -1,14 +1,19 @@
 package com.madiwist.twitch.presentation.login
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -36,27 +41,38 @@ fun LoginScreen(
     navController: NavController,
     viewModel: LoginViewModel = hiltViewModel()
 ) {
-    Box (
-        modifier = Modifier.fillMaxSize().padding(SpaceLarge)
-    ){
-        Column(modifier = Modifier.fillMaxSize().align(Alignment.Center),
-            verticalArrangement = Arrangement.Center
-        ){
+    val scrollState = rememberScrollState()
+
+    BoxWithConstraints(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(SpaceLarge)
+            .navigationBarsPadding()
+            .imePadding()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .defaultMinSize(minHeight = maxHeight)
+                .verticalScroll(scrollState),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Text(
                 text = stringResource(R.string.login),
                 color = MaterialTheme.colorScheme.onBackground,
-                style = MaterialTheme.typography.headlineLarge
+                style = MaterialTheme.typography.headlineLarge,
+                modifier = Modifier.fillMaxWidth()
             )
             Spacer(modifier = Modifier.height(ExtraSpaceLarge))
-            StandardTextField (
+            StandardTextField(
                 hint = stringResource(R.string.username_hint),
                 text = viewModel.username.value,
                 onValueChange = { viewModel.setUsername(it) },
                 error = viewModel.usernameError.value
-
             )
             Spacer(modifier = Modifier.height(SpaceMedium))
-            StandardTextField (
+            StandardTextField(
                 hint = stringResource(R.string.password_hint),
                 text = viewModel.password.value,
                 onValueChange = { viewModel.setPassword(it) },
@@ -69,41 +85,41 @@ fun LoginScreen(
             )
             Spacer(modifier = Modifier.height(ExtraSpaceLarge))
             Button(
-                onClick = {  },
+                onClick = { },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .align(Alignment.End)
                     .height(50.dp),
                 shape = RoundedCornerShape(10.dp),
                 elevation = ButtonDefaults.buttonElevation(
                     defaultElevation = 6.dp,
                     pressedElevation = 2.dp
-                ),
+                )
             ) {
                 Text(
-                    stringResource(R.string.login),
+                    text = stringResource(R.string.login),
                     fontWeight = FontWeight.Bold
                 )
             }
-        }
-        Text(
-            text = buildAnnotatedString {
-                append(stringResource(R.string.dont_have_an_account_yet))
-                append(" ")
-                val sigUp = stringResource(R.string.sign_up)
-                withStyle(
-                    style = SpanStyle(
-                        color = MaterialTheme.colorScheme.primary,
-                        fontWeight = FontWeight.Bold
-                    )
-                ) {
-                    append(sigUp)
-                }
-            },
-            style = MaterialTheme.typography.bodyLarge,
-            modifier = Modifier.align(
-                Alignment.BottomCenter
+
+            Spacer(modifier = Modifier.height(SpaceMedium))
+
+            Text(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                text = buildAnnotatedString {
+                    append(stringResource(R.string.dont_have_an_account_yet))
+                    append(" ")
+
+                    withStyle(
+                        SpanStyle(
+                            color = MaterialTheme.colorScheme.primary,
+                            fontWeight = FontWeight.Bold
+                        )
+                    ) {
+                        append(stringResource(R.string.sign_up))
+                    }
+                },
+                style = MaterialTheme.typography.bodyLarge
             )
-        )
+        }
     }
 }
